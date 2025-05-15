93f2b4228553f3bdd3125bfd3be2534b61eec2a1
Amisse Adamo <amisse.adamo@care.org>
2025-05-15 23:48:17 +0200

    Adiciona servidor express e corrige package.json

--- server/app.js
+++ server/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
+// Serve arquivos estáticos da pasta 'public'
 app.use(express.static(path.join(__dirname, '../public')));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
