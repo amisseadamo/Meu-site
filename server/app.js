e6386e6e682d2f0f7ac8fabf0af1d799b13df4c9
Amisse Adamo <amisse.adamo@care.org>
2025-05-16 00:32:06 +0200

    Corrige deploy: adiciona servidor e express

--- server/app.js
+++ server/app.js
@@ -4,13 +4,15 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-// Serve arquivos estáticos da pasta 'public'
+// Servir arquivos estáticos da pasta 'public'
 app.use(express.static(path.join(__dirname, '../public')));
 
+// Rota principal
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+// Iniciar servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
