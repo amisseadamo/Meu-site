722fe716eee88c77c088582b33dc697cb690c11e
Amisse Adamo <amisse.adamo@care.org>
2025-05-19 15:40:05 +0200

    atualizado

--- server/app.js
+++ server/app.js
@@ -7,10 +7,10 @@ const app = express();
 // Render exige que você use process.env.PORT
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 app.listen(PORT, () => {
