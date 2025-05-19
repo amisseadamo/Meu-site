b30e844b32e96c55bd047654171eeb86dab60a5f
Amisse Adamo <amisse.adamo@care.org>
2025-05-19 08:23:12 +0200

    Primeiro commit

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 
 const app = express();
+
+// Render exige que você use process.env.PORT
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '../public')));
